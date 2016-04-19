2bbc61961e19d5124661c716bd41ece7b64085c7
Rutul Patel <rap34002@uga.edu>
2016-04-18 23:23:07 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+	session_start();
+	$server = 'localhost';
+	$db_username = 'root';
+	$db_password = '';
+	$database = 'students';
+	
+		if(!mysql_connect($server, $db_username, $db_password)){
+			die('Could not connect to database!');
+		}
+		if(!mysql_select_db($database)){
+			die('Could not connect to the database');
+		}
+		
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<title>UGA Wall</title>
+    <script type="text/javascript" src="assets/js/jquery-2.2.1.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Welcome to UGA Wall</h2>
+  <form role="form">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+	<button type="submit" class="btn btn-default">Register</button>
+  </form>
+</div>
+
+</body>
+</html>
