ba2149a30bd485acedc87f1befc9faa15b7218a9
chiderek <chiderek@gmail.com>
2016-04-20 14:46:47 -0400

    connection to database and commenting out extra header

--- index.php
+++ index.php
@@ -1,31 +1,41 @@
-<?php 
+<?php
 	session_start();
 	$server = 'localhost';
 	$db_username = 'root';
 	$db_password = '';
 	$database = 'students';
-	
-		if(!mysql_connect($server, $db_username, $db_password)){
-			die('Could not connect to database!');
-		}
-		if(!mysql_select_db($database)){
-			die('Could not connect to the database');
-		}
-		
+
+try{
+    $conn = new PDO("mysql:host=$server;dbname=myMDb_db", $db_username, $db_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//    if(!mysql_connect($server, $db_username, $db_password)){
+//        die('Could not connect to database!');
+//    }
+//    if(!mysql_select_db($database)){
+//        die('Could not connect to the database');
+//    }
+
+}catch (PDOException $e){
+//    echo "Connection failed: " . $e->getMessage();
+}
+
+$conn = null;
 ?>
 
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-	<title>UGA Wall</title>
-    <script type="text/javascript" src="assets/js/jquery-2.2.1.min.js"></script>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-</head>
-<body>
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--	<title>UGA Wall</title>-->
+<!--    <script type="text/javascript" src="assets/js/jquery-2.2.1.min.js"></script>-->
+<!--    <link rel="stylesheet" href="assets/css/bootstrap.min.css">-->
+<!--</head>-->
+<!--<body>-->
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<!--    TODO: maybe update the jquery library-->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
