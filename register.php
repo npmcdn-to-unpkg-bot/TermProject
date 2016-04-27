8f1d487e6510d68e26c489b1239172e7984389ba
Andrew <andrewhsu007@gmail.com>
2016-04-27 15:12:58 -0400

    Changed css

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta charset="UTF-8">
-      <title>UGA Wall</title>
+      <title>Register</title>
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
       <link rel="stylesheet" href="assets/css/app.css">
     <!--    TODO: maybe update the jquery library-->
@@ -21,28 +21,30 @@
         <div class="container">
             <form class="form-signin" method="post" id="register-form" role="form">
                 <h2 class="form-signin-heading">Register</h2>
-                <hr>
                     <div id="error">
                     <!-- error will be shown here ! -->
                     </div>
 
                     <div class="form-group">
+                    <label for="username">Username:</label>
                         <input type="text" class="form-control" placeholder="Username" name="user_name" id="user_name" />
                     </div>
 
                     <div class="form-group">
+                    <label for="email">Email:</label>
                         <input type="email" class="form-control" placeholder="Email address" name="user_email" id="user_email" />
                         <span id="check-e"></span>
                     </div>
 
                     <div class="form-group">
+                    <label for="pwd">Password:</label>
                         <input type="password" class="form-control" placeholder="Password" name="password" id="password" />
                     </div>
 
                     <div class="form-group">
+                    <label for="pwd">Password:</label>
                         <input type="password" class="form-control" placeholder="Retype Password" name="cpassword" id="cpassword" />
                     </div>
-                <hr>
                 <div class="form-group">
                     <button type="submit" class="btn btn-default" name="btn-save" id="btn-submit">
                         <span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account
