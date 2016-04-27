8f0e6394a2b1cb084319e89983e78f320b82021c
Andrew <andrewhsu007@gmail.com>
2016-04-26 22:33:52 -0400

    Added upvote plugin to index.php under first two columns

--- index.php
+++ index.php
@@ -14,10 +14,12 @@ include 'DbConfig.php';
     <title>UGA Wall</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.upvote.css">
     <link rel="stylesheet" href="assets/css/app.css">
     <!--    TODO: maybe update the jquery library-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.upvote.js"></script>
 </head>
 <body>
 <?php
@@ -32,12 +34,22 @@ include 'navbar.html';
                 <a href="images/corgi.jpg">
                     <img src="images/corgi.jpg" alt="Corgi4Lyfe">
                 </a>
+                <div id="c1" class="upvote">
+                    <a class="upvote"></a>
+                    <span class="count">0</span>
+                    <a class="downvote"></a>
+                </div>
             </div>
             <div class="pin" style="background-color:red">
                 <!--placeholder to see how linking to an article may look-->
                 <a href="http://www.w3schools.com/" alt="w3">
                     <div class="linkContainer">W3schools</div>
                 </a>
+                <div id="c2" class="upvote">
+                    <a class="upvote"></a>
+                    <span class="count">0</span>
+                    <a class="downvote"></a>
+                </div>
             </div>
             <div class="pin" style="background-color:yellow">
                 col-lg-4
@@ -69,5 +81,10 @@ include 'navbar.html';
 // close connection
 $conn = null;
 ?>
+
+    <script>
+    $('#c1').upvote();
+    $('#c2').upvote();
+    </script>
 </body>
 </html>
