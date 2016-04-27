d6aebb3a9f8b6f7ef1861f4d08a07ed77fc2d7b6
Andrew <andrewhsu007@gmail.com>
2016-04-27 14:52:01 -0400

    Changed css for login and register

--- register.php
+++ register.php
@@ -18,36 +18,38 @@
         <?php
             include 'navbar.html';
         ?>
-        <form class="form-signin" method="post" id="register-form" role="form">
-            <h2 class="form-signin-heading">Register</h2>
-            <hr>
-                <div id="error">
-                <!-- error will be shown here ! -->
-                </div>
+        <div class="container">
+            <form class="form-signin" method="post" id="register-form" role="form">
+                <h2 class="form-signin-heading">Register</h2>
+                <hr>
+                    <div id="error">
+                    <!-- error will be shown here ! -->
+                    </div>
 
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Username" name="user_name" id="user_name" />
-                </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Username" name="user_name" id="user_name" />
+                    </div>
 
-                <div class="form-group">
-                    <input type="email" class="form-control" placeholder="Email address" name="user_email" id="user_email" />
-                    <span id="check-e"></span>
-                </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Email address" name="user_email" id="user_email" />
+                        <span id="check-e"></span>
+                    </div>
 
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password" name="password" id="password" />
-                </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password" name="password" id="password" />
+                    </div>
 
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Retype Password" name="cpassword" id="cpassword" />
+                    </div>
+                <hr>
                 <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Retype Password" name="cpassword" id="cpassword" />
+                    <button type="submit" class="btn btn-default" name="btn-save" id="btn-submit">
+                        <span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account
+                    </button>
                 </div>
-            </hr>
-            <div class="form-group">
-                <button type="submit" class="btn btn-default" name="btn-save" id="btn-submit">
-                    <span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account
-                </button>
-            </div>
-        </form>
+            </form>
+        </div>
         <?php
         // close connection
         $conn = null;
