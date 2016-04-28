9eafca16a0b89e424aeb6f6b60d2b001b4558410
chiderek <chiderek@gmail.com>
2016-04-27 23:27:44 -0400

    implementing cycle post script to display each post

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ include 'DbConfig.php';
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.upvote.js"></script>
+    <script src="assets/js/facebook-login.js"></script>
 
     <script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
     <script src="assets/js/masonry.pkgd.js"></script>
@@ -24,46 +25,49 @@ include 'navbar.html';
 ?>
 
 <div class="grid" id="mainContent">
-    <div class="grid-item">
-        <a href="images/corgi.jpg" alt="Corgi4Lyfe">
-            <img src="images/corgi.jpg" alt="Corgi4Lyfe">
-        </a>
-        <div id="c1" class="upvote">
-            <a class="upvote"></a>
-            <span class="count">0</span>
-            <a class="downvote"></a>
-        </div>
-    </div>
-    <div class="grid-item">
-        <a href="http://www.w3schools.com/" alt="w3">
-            <div class="linkContainer">W3schools</div>
-        </a>
-        <div id="c2" class="upvote">
-            <a class="upvote"></a>
-            <span class="count">0</span>
-            <a class="downvote"></a>
-        </div>
-    </div>
-    <div class="grid-item">
-        <a href="images/out-after-shuffle.png">
-            <img src="images/out-after-shuffle.png" alt="">
-        </a>
-    </div>
-    <div class="grid-item">
-        <a href="images/out-after-shuffle.png">
-            <img src="images/out-after-shuffle.png" alt="">
-        </a>
-    </div>
-    <div class="grid-item">
-        <a href="images/out-after-shuffle.png">
-            <img src="images/out-after-shuffle.png" alt="">
-        </a>
-    </div>
-    <div class="grid-item">
-        <a href="images/out-after-shuffle.png">
-            <img src="images/out-after-shuffle.png" alt="">
-        </a>
-    </div>
+    <?php
+
+    // get all posts
+    $host     = "localhost";
+    $port     = 3306;
+    $socket   = "";
+    $dbname   = "uga_wall";
+    $connectStatus = "";
+
+    $conn = new mysqli($host, "root", "", $dbname, $port, $socket);
+
+    if ($conn->connect_error)
+    {
+        //header("Location: /www/Protelog/Views/Account/error.php");
+    }
+
+
+    // select users.username, posts.message from users join posts on users.userID = posts.userID
+
+    // Join users and posts tables to match username to message
+    $stmtSelect = $conn->prepare("select users.username, posts.message from users join posts on users.userID = posts.userID");
+    $stmtSelect->execute();
+    $stmtSelect->bind_result($username, $message);
+
+    // counts how many rows have been counted to put into a <div class="row">
+    $switchCounter = 0;
+
+    while ($stmtSelect->fetch())
+    {
+        echo '<div class="grid-item">
+                <div>' . $message . '</div>
+                <p><i>By user - ' . $username . '</i></p>
+                <div id="c2" class="upvote">
+                    <a class="upvote"></a>
+                    <span class="count">0</span>
+                    <a class="downvote"></a>
+                </div>
+              </div>';
+    }
+
+    $stmtSelect->close();
+    $conn->close();
+    ?>
 </div>
 
 <?php
