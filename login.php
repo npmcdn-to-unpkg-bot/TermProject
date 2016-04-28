f67cbaf9e7a4cffac3c8bb14ecb82af2d99086c8
dbalbed1 <dbalbed1@uga.edu>
2016-04-27 23:52:23 -0400

    Now works with login_handle

--- login.php
+++ login.php
@@ -1,6 +1,3 @@
-<?php
-include 'DbConfig.php';
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,14 +15,14 @@ include 'DbConfig.php';
     ?>
     <div class="container">
       <h2>Welcome to UGA Wall</h2>
-      <form role="form">
+      <form action="login_handle.php" method="POST">
         <div class="form-group">
           <label for="email">Email:</label>
-          <input type="email" class="form-control" id="email" placeholder="Enter email">
+          <input name="email" type="email" class="form-control" id="email" placeholder="Enter email">
         </div>
         <div class="form-group">
           <label for="pwd">Password:</label>
-          <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+          <input name="password" type="password" class="form-control" id="password" placeholder="Enter password">
         </div>
         <div class="checkbox">
           <label><input type="checkbox"> Remember me</label>
