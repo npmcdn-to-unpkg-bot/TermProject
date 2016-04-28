66912e3cb731ed7dc9038f632ebb6cc1be5c4337
chiderek <chiderek@gmail.com>
2016-04-28 00:00:16 -0400

    commenting out script tag for facebook login.js

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include 'DbConfig.php';
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.upvote.js"></script>
-    <script src="assets/js/facebook-login.js"></script>
+<!--    <script src="assets/js/facebook-login.js"></script>-->
 
     <script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
     <script src="assets/js/masonry.pkgd.js"></script>
